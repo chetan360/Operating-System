72f3e0d28334cf10f2ecc53ce79be4d615e53765
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2023-11-27 20:49:59 +0530

    Update Asg2C1.c

--- os/Asg2 Operations on Processes/Asg2C1.c
+++ os/Asg2 Operations on Processes/Asg2C1.c
@@ -7,80 +7,44 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-void typeline(char *s, char *fn) {
-	int handle, i=0, cnt=0, n;
+void typeline(char *s, char *filename) {
+	int fp, i=0, n;
 	char ch;
-
-	//open the file 
-	if((handle = open(fn, O_RDONLY)) == -1) {
-		printf("File %s not found\n", fn);
+	
+	//open the file
+	if((fp = open(filename, O_RDONLY)) == -1) {
+		printf("File %s not found\n", filename);
 		return;
 	}
 	
-	if(strcmp(s, "a") == 0) {
-		//printing till end of file is not occurred 
-		while(read(handle, &ch, 1) != 0) {
+	if(strcmp(s, "-a") == 0) {
+		while(read(fp, &ch, 1) != 0) {
 			printf("%c", ch);
 		}
-		printf("\n");
+		printf("\n\n");
 		//close file
-		close(handle);
+		close(fp);
 		return;
 	}
 	
-	n = atoi(s);
+	n = atoi(s); 
 	if(n > 0) {
-		//1. print from frist line and count
-		//   no. of lines in i
-		while(read(handle, &ch, 1) != 0) {
+		//print from first line and count
+		//no. of lines in i
+		while(read(fp, &ch, 1) != 0) {
 			if(ch == '\n') {
 				i++;
 			}
-			//2. i becomes n the stop printing
 			if(i == n) {
 				break;
 			}
 			printf("%c", ch);
 		}
 		printf("\n\n");
-
+		
 		//close file
-		close(handle);
+		close(fp);
 		return;
-	}
-	
-	if(n < 0) {
-		//1. count no. of lines
-		while(read(handle, &ch, 1) != 0) {
-			if(ch == '\n') {
-				cnt++;
-			}
-		}
-
-		//file read location is reached to end line
-		//so seek at first line
-		lseek(handle, 0, SEEK_SET);
-
-		//2. seeking file read location to 
-		//   no. of lines + (-n) 
-		while(read(handle, &ch, 1) != 0) {
-			if(ch == '\n') {
-				i++;
-			}
-			if(i == cnt+n) {
-				break;
-			}
-		}
-
-		//3. printing from that last -n lines
-		while(read(handle, &ch, 1) != 0) {
-			printf("%c", ch);
-		}
-		printf("\n");
-
-		//close file
-		close(handle);
-		return;		
 	}
 }
 
@@ -89,7 +53,6 @@ int main() {
 	int n;
 	
 	system("clear");
-	
 	while(1) {
 		printf("myShell$ : ");
 		fflush(stdin);
@@ -101,14 +64,13 @@ int main() {
 			if(strcmp(t1, "typeline") == 0) {
 				typeline(t2, t3);
 			} else {
-				printf("%s is not valid cmd.\n", t1);
+				printf("%s is invalid command.\n", t1);
 			}
 		} else {
-			printf("please enter (typeline n/-n/a filename) cmd in this format.\n");
+			printf("please enter (typeline +n/-a filename) command in this form.\n ");
 		}
 	}
-	
+
 	return 0;
 }
-				
 
